30696448c1f4ea723b36db2f79c4fe1edab7ae99
Jaimes77 <JaimeRGoff@gmail.com>
2021-08-31 20:02:46 -0400

    first commit, userController-completed Signup and Login

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("iSpendServer", "postgres", "password", {
-    host: "localhost",
-    dialect: "postgres",
+const sequelize = new Sequelize("iSpendServer", "postgres", "Marz*ipan143", {
+  host: "localhost",
+  dialect: "postgres",
 });
 
 sequelize
-    .authenticate()
-    .then(() => {
-        console.log("Connection has been established successfully.");
-    })
-    .catch((err) => {
-        console.error('Unable to connect to the database:', err);
-    });
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
-    module.exports = sequelize;
+module.exports = sequelize;
