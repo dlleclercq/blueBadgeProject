d0d8dd80a816fa6426cab6a463d1a84a30b826e4
Jason-Henson <jason.a.henson@gmail.com>
2021-08-31 20:12:31 -0400

    first push

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 let express = require("express");
-const app = express();
 const sequelize = require("./db");
-
-
+const app = express();
+app.use(require('./middleware/headers'));
 sequelize.sync();
-
 app.use(express.json());
-
+const user = require('./controllers/userController');
+const expense = require('./controllers/expenseController');
+app.use('/user', user);
+app.use('/expense', expense);
 app.listen(3000, function () {
     console.log("App is listening on port 3000");
 })
+
+module.exports = router; 
