a894787fdba453fccf2f60d796e6bc04788db01a
Jason-Henson <jason.a.henson@gmail.com>
2021-09-24 22:11:44 -0400

    final push

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ require("dotenv").config();
 let express = require("express");
 const sequelize = require("./db");
 const app = express();
-// app.use(require("./middleware/headers"));
+app.use(require("./middleware/headers"));
 sequelize.sync();
 app.use(express.json());
 const user = require("./controllers/userController");
 const expense = require("./controllers/expenseController");
 app.use("/user", user);
 app.use("/expense", expense);
-app.listen(3000, function () {
-  console.log("App is listening on port 3000");
-});
+// If your using express to listen on a port it will be app.listen. 
+// If your using node http to listen on a port it will be http.listen.  
+app.listen(process.env.PORT, () => {
+  console.log(`server is listening on port ${process.env.PORT}`)
+})
--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("iSpendServer", "postgres", "password", {
-  host: "localhost",
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+})
 
 sequelize
   .authenticate()
